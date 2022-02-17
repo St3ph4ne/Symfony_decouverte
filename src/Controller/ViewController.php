03908a0131b4922a81b49c29bb662acee9d564e9
St3ph4ne <spodar.s@protonmail.com>
2022-02-17 17:39:23 +0000

    concert creation tables et view

--- src/Controller/ViewController.php
+++ src/Controller/ViewController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\UserRepository;
+use App\Repository\PersonneRepository;
 
 class ViewController extends AbstractController
 {
@@ -30,6 +31,15 @@ class ViewController extends AbstractController
         $CONTEXT = ['users'=>$users];
 
         return $this->render('view/users.html.twig', $CONTEXT);
+    }
+
+    #[Route('/personnes', name: 'personnes')]
+    public function getPersonnes(PersonneRepository $repo): Response
+    {
+        $personnes = $repo->getAll();
+        $CONTEXT = ['personnes'=>$personnes];
+
+        return $this->render('view/personnes.html.twig', $CONTEXT);
     }
 
 }
